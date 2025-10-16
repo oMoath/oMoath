83dea3b4514b5bf9603ea2a01fcbada9759ffbb3
Moath <115834773+oMoath@users.noreply.github.com>
2025-10-16 22:27:03 +0300

    Update and rename page-a.js to page-a2.js

--- chunks/app/page-a2.js
+++ chunks/app/page-a2.js
@@ -473,7 +473,6 @@
                 }, e))
             })
         }
-        let b = [];
         var v = a(5430);
         function w() {
             let[e,t] = (0,
@@ -522,6 +521,7 @@
                     }), (0,
                     l.jsx)("div", {
                         className: "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6",
+                        children: null
                     }), 0 === u.length && (0,
                     l.jsx)("div", {
                         className: "text-center py-12",
